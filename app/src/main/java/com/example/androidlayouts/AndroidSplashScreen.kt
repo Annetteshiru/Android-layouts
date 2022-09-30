53c376cd9850ce3c70a2cc4e766569fed2ba23f6
Annetteshiru <awanjiruwambui75@gmail.com>
2022-09-30 14:40:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidlayouts/AndroidSplashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.androidlayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AndroidSplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_android_splash_screen)
+    }
+}
---
+++ app/src/main/java/com/example/androidlayouts/Androidlayout.kt
@@ -0,0 +1,12 @@
+package com.example.androidlayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Androidlayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_androidlayout2)
+    }
+
+}
